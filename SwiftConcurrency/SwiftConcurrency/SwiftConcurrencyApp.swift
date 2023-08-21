b9e44d93470ddc8495a0c5eb488bd6a70eb39e8e
manishparihar948 <manish.parihar948@gmail.com>
2023-08-21 11:32:25 +0200

    Initical Commit

---
+++ SwiftConcurrency/SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Manish Parihar on 21.08.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
